cc303295154a2fdf5db8f71666650abc0b2c8acc
Thiago Borges <t.borges@live.com>
2026-03-30 23:42:04 -0300

    Criação da entidade Address e conexão com banco de dados

--- src/address/entities/address.entity.ts
+++ src/address/entities/address.entity.ts
@@ -1,13 +1,72 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+
 export class AddressEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @CreateDateColumn()
   createdAt: Date;
-  status: number;
-  countryCode: number;
+
+  @UpdateDateColumn()
+  updateAt: Date;
+
+  @Column({
+    type: "boolean",
+    default: true,
+  })
+  status: boolean;
+
+  @Column({
+    type: "char",
+    length: 2,
+    name: "country_code",
+    default: "BR",
+  })
+  countryCode: string;
+
+  @Column({
+    type: "varchar",
+    length: 50,
+  })
   state: string;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+  })
   city: string;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+  })
   district: string;
+
+  @Column({
+    type: "varchar",
+    length: 150,
+  })
   street: string;
-  number: number;
+
+  @Column({
+    type: "varchar",
+    length: 10,
+  })
+  number: string;
+
+  @Column({
+    type: "varchar",
+    length: 150,
+    nullable: true,
+  })
   complement: string;
+
+  @Column({
+    type: "char",
+    length: 8,
+    name: "postal_code",
+  })
   postalCode: string;
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AddressController } from './address/address.controller';
+import { AddressModule } from './address/address.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 
 @Module({
-  imports: [AddressController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'animalpontocom',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    AddressModule],
   controllers: [AppController],
   providers: [AppService],
 })
